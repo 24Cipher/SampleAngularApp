49b1574cba9d18367f7844dddb19ad6624d9985b
jayant <jayantojha9@gmail.com>
2020-09-01 05:09:35 +0530

    add child components in home component

--- src/app/show-profile/show-profile.component.ts
+++ src/app/show-profile/show-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PrototypeService } from '../services/prototype.service';
 
 @Component({
   selector: 'app-show-profile',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: PrototypeService) { }
 
   ngOnInit(): void {
+    this.readProfiles();
   }
 
+  readProfiles(): void {
+    this.service.readChildProfiles().subscribe(resp => console.log(resp))
+  }
 }
