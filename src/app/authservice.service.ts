619b3ad10561a82c807ecbadea9d2849a91f3948
jayant <jayantojha9@gmail.com>
2020-09-01 05:06:39 +0530

    (fix) maintaing user session

--- src/app/authservice.service.ts
+++ src/app/authservice.service.ts
@@ -1,19 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { PrototypeService } from './services/prototype.service';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
+import { User } from 'src/app/shared/services/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthserviceService {
   isSignedIn = false;
-  loggedInUserId
-  loggedInEmail
+  loggedInUserId: any;
+  loggedInEmail: any;
+  userData: any; // Save logged in user data
 
-  constructor(public router: Router, public afAuth: AngularFireAuth, public firestore: AngularFirestore) {
+  constructor(public router: Router, public afAuth: AngularFireAuth, public firestore: AngularFirestore,  public ngZone: NgZone ) {
     this.afAuth.user.subscribe(res=>{
       if(res.uid){
         this.loggedInUserId = res.uid
@@ -24,12 +26,26 @@ export class AuthserviceService {
       else{
         this.isSignedIn = false
       }
+    });
+
+    /* Saving user data in localstorage when 
+    logged in and setting up null when logged out */
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user'));
+      } else {
+        localStorage.setItem('user', null);
+        JSON.parse(localStorage.getItem('user'));
+      }
     })
   }
 
   signInAuth(email, password){
 
     this.afAuth.signInWithEmailAndPassword(email, password).then(res=>{
+      // this.SetUserData(res.user);
       this.isSignedIn = true
       this.loggedInUserId  = res.user.uid
       this.loggedInEmail = email
@@ -51,9 +67,12 @@ export class AuthserviceService {
 
   LogoutAuth(){
     this.isSignedIn = false
-    this.afAuth.signOut()
-    location.reload()
-    this.router.navigateByUrl("/signin")
+    this.afAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['signin']);
+    });
+    // location.reload()
+    // this.router.navigateByUrl("/signin")
   }
 
   isUserSignedIn(){
@@ -61,7 +80,7 @@ export class AuthserviceService {
   }
 
   readUserDetails(){
-    debugger;
+    // debugger;
     if(this.isSignedIn){
       var user = this.firestore.collection("User", ref=>ref.where('email', '==', this.getUserEmail()))
       user.snapshotChanges().pipe(
@@ -92,6 +111,7 @@ export class AuthserviceService {
       this.loggedInUserId  = res.user.uid
       this.loggedInEmail = email
       this.readUserDetails();
+      // this.SetUserData(res.user);
       this.router.navigateByUrl("/home")
     }).catch(err=>{
       alert(err)
@@ -106,5 +126,22 @@ export class AuthserviceService {
     return this.loggedInEmail
   }
 
+
+   /* Setting up user data when sign in with username/password, 
+  sign up with username/password and sign in with social auth  
+  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
+  SetUserData(user) {
+    const userRef: AngularFirestoreDocument<any> = this.firestore.doc(`users/${user.uid}`);
+    const userData: User = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      emailVerified: user.emailVerified
+    }
+    return userRef.set(userData, {
+      merge: true
+    })
+  }
 
 }
